68918f11f4ca292368f3a2c85a097ab3678eb620
edi <BBerastegui@users.noreply.github.com>
2015-03-30 19:04:58 +0200

    Added my PoC with channels and stuff with GoLang

---
+++ proxyscan.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+	"time"
+)
+
+func random(min, max int) int {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(max-min) + min
+}
+
+var done chan bool
+
+// Channel of simultaneous tasks
+var simul = make(chan string, 5)
+
+// Array of strings with finished tasks
+var finished_tasks []string
+
+func main() {
+
+	task_queue := make(chan string)
+	//var task_run chan string
+
+	go consume(task_queue, simul)
+	go feed(task_queue)
+	<-done
+	fmt.Println("Tareas finalizadas: ", finished_tasks)
+
+}
+
+// This function will feed the task channel
+func feed(task_queue chan string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println("Adding 10 more tasks...")
+		for x := 0; x < 10; x++ {
+			task_queue <- strconv.FormatInt(time.Now().UnixNano()/int64(time.Microsecond), 10)
+		}
+		fmt.Println("Waiting 10 seconds to add more tasks...")
+		time.Sleep(time.Second * 10)
+	}
+	done <- true
+}
+
+func consume(task_queue chan string, simul chan string) {
+	for {
+		id := <-task_queue
+		simul <- id
+		go scan(id, simul)
+	}
+}
+
+func scan(id string, simul chan string) {
+	fmt.Println("Scan commenced...", id)
+	wait := random(1, 5)
+	fmt.Println("Waiting for ", wait, " seconds.")
+	time.Sleep(time.Second * time.Duration(wait))
+	fmt.Println("Task ", id, " finished.")
+	fin_task := <-simul
+	finished_tasks = append(finished_tasks, fin_task)
+}
