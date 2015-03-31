a6c7f9dde77fbcd80993a20a41a07dbc10a86598
edi <BBerastegui@users.noreply.github.com>
2015-03-31 19:05:03 +0200

    Added moar... things.

--- proxyscan.go
+++ proxyscan.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math/rand"
-	"strconv"
+	"net"
 	"sync"
 	"time"
 )
@@ -13,55 +14,64 @@ func random(min, max int) int {
 	return rand.Intn(max-min) + min
 }
 
+func inc(ip net.IP) {
+	for j := len(ip) - 1; j >= 0; j-- {
+		ip[j]++
+		if ip[j] > 0 {
+			break
+		}
+	}
+}
+
 // Channel of simultaneous tasks
-var simul = make(chan string, 5)
+var simul = make(chan string, 20)
+var task_queue = make(chan string)
 
 // Array of strings with finished tasks
 var finished_tasks []string
 
-// Waitgroup... or something...
+// Add dummy waitgroup in order for all to be running forever
 var wg sync.WaitGroup
 
 func main() {
-	wg.Add(1)
-	task_queue := make(chan string)
-	//var task_run chan string
-
-	go consume(task_queue, simul)
 	go feed(task_queue)
-	wg.Wait()
+	consume(task_queue, simul)
+
 	fmt.Println(len(finished_tasks), " tareas finalizadas: ", finished_tasks)
 }
 
 // This function will feed the task channel
 func feed(task_queue chan string) {
-	for i := 0; i < 3; i++ {
-		fmt.Println("Adding 10 more tasks...")
-		for x := 0; x < 10; x++ {
-			task_queue <- strconv.FormatInt(time.Now().UnixNano()/int64(time.Microsecond), 10)
-		}
-		fmt.Println("Waiting 10 seconds to add more tasks...")
-		time.Sleep(time.Second * 10)
+
+	ip, ipnet, err := net.ParseCIDR("10.76.22.0/24")
+	if err != nil {
+		log.Fatal(err)
 	}
-	wg.Done()
+	for ip := ip.Mask(ipnet.Mask); ipnet.Contains(ip); inc(ip) {
+		task_queue <- ip.String()
+	}
+	fmt.Println("FEED FINISHED")
 }
 
 // This will run in a loop 4evah, feeding as simultaneous goroutines
 // as the simul channel buffer is set.
 func consume(task_queue chan string, simul chan string) {
 	for {
-		id := <-task_queue
-		simul <- id
-		go scan(id, simul)
+		select {
+		case ip := <-task_queue:
+			//ip := <-task_queue
+			simul <- ip
+			go scan(ip, simul)
+		}
 	}
 }
 
-func scan(id string, simul chan string) {
-	fmt.Println("Scan commenced...", id)
+func scan(ip string, simul chan string) {
+	// Foo demo func.
+	fmt.Println("Scan commenced...", ip)
 	wait := random(1, 5)
 	fmt.Println("Waiting for ", wait, " seconds.")
 	time.Sleep(time.Second * time.Duration(wait))
-	fmt.Println("Task ", id, " finished.")
-	fin_task := <-simul
-	finished_tasks = append(finished_tasks, fin_task)
+	fmt.Println("Task ", ip, " finished.")
+	finished_tasks = append(finished_tasks, <-simul)
 }
